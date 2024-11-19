7fd232996f25b8796009d66f038b6f4aa37a2498
Arkan-21 <A.W.Skibinska@student.tudelft.nl>
2024-11-19 15:51:23 +0100

    Update wp4-2.py

--- WP4-5/wp4-2.py
+++ WP4-5/wp4-2.py
@@ -3,5 +3,8 @@ print("Hello World")
 def ChordLength(Tr, Cr, b):
     c = Cr - Cr*(1-Tr)
 
-def Centroid(c):
-    c=1
+def Centroid(c, t):
+    A1 = 0.1013*c*t
+    A2 = 0.55*c*alpha*t
+    A3 = 0.1013*c*t
+    A4 = 0.1013*c*t
