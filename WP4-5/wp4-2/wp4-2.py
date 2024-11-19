ee7dee8919b56bf823120e32c9c1c57cd2955999
Federico Angioni <f.angioni@student.tudelft.nl>
2024-11-19 16:31:27 +0100

    commit

--- WP4-5/wp4-2/wp4-2.py
+++ WP4-5/wp4-2/wp4-2.py
@@ -5,6 +5,7 @@ class WingBox(self):
     def chord(z, c_r, c_t):
         c = c_r - c_r*(1-(c_t/c_r))*z
         return(c)
+    
     def Centroid(c, t):
         A1 = 0.1013*c*t
         A2 = 0.55*c*alpha*t
