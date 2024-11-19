fb27acbe25b37d47cf6d1b402ce81e3fb07be214
winston1010 <w.z.h.wang@student.tudelft.nl>
2024-11-19 17:23:37 +0100

    test

--- WP4-5/wp4-2/wp4-2.py
+++ WP4-5/wp4-2/wp4-2.py
@@ -7,27 +7,26 @@ class WingBox(self):
         c = c_r - c_r*(1-(c_t/c_r))*z
         return(c)
     
-    def Centroid(self, c, t, alpha, stringer_x_pos[], stringer_y_pos[], stringer_area[]):
-        A = [0.0728*c*t, 0.1013*c*t, 0.55*c*np.sin(np.radians(alpha))*t, 0.55*c*np.sin(np.radians(alpha))*t] #Areas of the components
-        X = [0, 0.55*c, 0.5*0.55*c*np.cos(np.radians(alpha)), 0.5*0.55*c*np.cos(np.radians(alpha))] # X positions of the components
-        Y = [0, 0, 0.5*0.1013*c-0.5*0.55*c*np.sin(np.radians(alpha)), -0.5*0.1013*c+0.5*0.55*c*np.sin(np.radians(alpha))] # Y positions of the components
-        while j <= len(stringer_x_pos): #include the contributions of the stringers
+    def Centroid(self, c, t, alpha, stringer_x_pos[], stringer_area[]):
+        A = [0.0728*c*t, 0.1013*c*t, 0.55*c*np.sin(np.radians(alpha))*t, 0.55*c*np.sin(np.radians(alpha))*t] #area 
+        X = [0, 0.55*c, 0.5*0.55*c*np.cos(np.radians(alpha)), 0.5*0.55*c*np.cos(np.radians(alpha))]
+
+        while j <= len(stringer_x_pos)
             A.append(stringer_area[j])
             X.append(stringer_x_pos[j])
-            Y.append(stringer_y_pos[j])
 
         while i <= len(X):
-            weights_X = A[i]*X[i]
-            weights_Y = A[i]*Y[i]
+            weights = A[i]*A[X]
         
-        x = (weights_X)/sum(A)
-        y= (weights_Y)/sum(A)
-        return(x, y)
-    
 
+        x = (weights)/sum(A)
+
+    def MOMEWB (self,):
+        
         
 
 
 
 
 wingbox = WingBox()
+
