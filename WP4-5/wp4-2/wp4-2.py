d8ea954bee0f00169074c67072b132eee91a169f
Federico Angioni <f.angioni@student.tudelft.nl>
2024-11-19 16:46:06 +0100

    changes

--- WP4-5/wp4-2/wp4-2.py
+++ WP4-5/wp4-2/wp4-2.py
@@ -1,6 +1,9 @@
 import scipy as sc
+import numpy as np
 
 class WingBox(self):
+    def __init__(self):
+        pass
     
     def chord(z, c_r, c_t):
         c = c_r - c_r*(1-(c_t/c_r))*z
@@ -14,8 +17,9 @@ class WingBox(self):
 
         x = (A1*0 + A2*0.55*c + 0.5*0.55*c*np.cos(np.radians(alpha)) + 0.5*0.55*c*np.cos(np.radians(alpha)))/(A1+A2+A3+A4)
         
+    
 
-
+    
 
 
 wingbox = WingBox()
