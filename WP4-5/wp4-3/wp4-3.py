2dc393bcb8e17f4520be790a39b85b8cde59dbda
MineMathijs <mathijs.wijkmans@gmail.com>
2024-11-19 16:04:50 +0100

    n_max formula

--- WP4-5/wp4-3/wp4-3.py
+++ WP4-5/wp4-3/wp4-3.py
@@ -4,6 +4,9 @@ import math
 def speed_from_lift(Lift, rho, S, Cl):
     return math.sqrt((2 * Lift) / (rho * S * Cl))
 
+def n_max(weight_kg):
+    kg_to_lbs = 2.20462
+    return 2.1 + (2400 / ((kg_to_lbs * weight_kg) + 1000))
 
 if __name__ == "__main__":
     print("running wp4-3.py")
