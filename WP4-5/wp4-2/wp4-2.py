b5f32bd8807546d30ec4779722350fd63137f3eb
Federico Angioni <f.angioni@student.tudelft.nl>
2024-11-19 16:37:26 +0100

    stiffeners

--- WP4-5/wp4-2/wp4-2.py
+++ WP4-5/wp4-2/wp4-2.py
@@ -1,15 +1,23 @@
 import scipy as sc
 
-class WingBox(self):
+class WingBox():
+    def __init__(self):
+        pass
     
-    def chord(z, c_r, c_t):
+    def chord(self, z, c_r, c_t):
         c = c_r - c_r*(1-(c_t/c_r))*z
         return(c)
     
-    def Centroid(c, t):
+    def Centroid(self, c, t):
         A1 = 0.1013*c*t
         A2 = 0.55*c*alpha*t
         A3 = 0.1013*c*t
         A4 = 0.1013*c*t
 
+    def stiffener(self, number, type):
+        type = ["L", "I", ]
+        
+        
+        
+        
 wingbox = WingBox()
