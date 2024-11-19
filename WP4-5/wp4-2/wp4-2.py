f29f80a1caf3027236bb18ab247ad4809361b2fb
Arkan-21 <A.W.Skibinska@student.tudelft.nl>
2024-11-19 16:42:00 +0100

    centroid

--- WP4-5/wp4-2/wp4-2.py
+++ WP4-5/wp4-2/wp4-2.py
@@ -1,23 +1,21 @@
 import scipy as sc
 
-class WingBox():
-    def __init__(self):
-        pass
+class WingBox(self):
     
-    def chord(self, z, c_r, c_t):
+    def chord(z, c_r, c_t):
         c = c_r - c_r*(1-(c_t/c_r))*z
         return(c)
     
-    def Centroid(self, c, t):
-        A1 = 0.1013*c*t
-        A2 = 0.55*c*alpha*t
-        A3 = 0.1013*c*t
-        A4 = 0.1013*c*t
+    def Centroid(selfc, t, alpha):
+        A1 = 0.0728*c*t
+        A2 = 0.1013*c*t
+        A3 = 0.55*c*np.sin(np.radians(alpha))*t
+        A4 = A3
 
-    def stiffener(self, number, type):
-        type = ["L", "I", ]
+        x = (A1*0 + A2*0.55*c + 0.5*0.55*c*np.cos(np.radians(alpha)) + 0.5*0.55*c*np.cos(np.radians(alpha)))/(A1+A2+A3+A4)
         
-        
-        
-        
+
+
+
+
 wingbox = WingBox()
