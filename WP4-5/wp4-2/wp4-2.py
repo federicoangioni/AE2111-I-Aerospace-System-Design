f7ae30e15f16c81212a627a41315ae61a3dcf57f
wilsobh <sirhamsworth@gmail.com>
2024-11-19 17:10:08 +0100

    asdfghjk

--- WP4-5/wp4-2/wp4-2.py
+++ WP4-5/wp4-2/wp4-2.py
@@ -5,17 +5,21 @@ class WingBox(self):
     def __init__(self):
         pass
     
-    def chord(z, c_r, c_t):
-        c = c_r - c_r*(1-(c_t/c_r))*z
+    def chord(self, z, c_r, c_t, b): #
+        c = c_r - c_r*(1-(c_t/c_r))*z/(b/2)
         return(c)
     
-    def Centroid(selfc, t, alpha):
+    def Centroid(self, c, t, alpha):
         A1 = 0.0728*c*t
         A2 = 0.1013*c*t
         A3 = 0.55*c*np.sin(np.radians(alpha))*t
         A4 = A3
 
         x = (A1*0 + A2*0.55*c + 0.5*0.55*c*np.cos(np.radians(alpha)) + 0.5*0.55*c*np.cos(np.radians(alpha)))/(A1+A2+A3+A4)
+
+    
+
+    
         
     
 
