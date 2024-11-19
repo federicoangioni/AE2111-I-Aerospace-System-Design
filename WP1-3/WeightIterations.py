b9259d2eb7485c019fff6d22d8153601c291556c
Federico Angioni <f.angioni@student.tudelft.nl>
2024-11-19 14:31:10 +0100

    commit

--- WP1-3/WeightIterations.py
+++ WP1-3/WeightIterations.py
@@ -1,37 +1,23 @@
 import numpy as np
 import subprocess
-from class_2_weight import OEW_est, update_OEW
-from class_1_weight import predict
-from variables import MTOW
-from SARoptimization import optimized_AR, optimized_e, optimized_S
-
+from class_2_weight import MTOW, OEW_est
 
 files_tobe_Updated = [
     "class_1_weight.py", "MatchingDiagramPlot.py", "planform.py", "SARoptimization.py",
-    "CG_location.py", "fuselage.py", "class_2_weight.py", "empennage_planform.py", "class_2_drag.py"
-]
+    "CG_location.py", "fuselage.py", "empennage_planform.py", "class_2_weight.py", "class_2_drag.py"
+    ]
 
 Weights = []
 
-for i in range(20):
+for i in range(10):
     print(f"Current OEW is: {OEW_est}, current MTOW is {MTOW}")
     MTOW = predict(OEW_est)
     Weights.append(MTOW)
 
-    # Update CG_location.py with the new OEW_mass_fraction
-    with open("SARoptimization.py", 'r') as file:
-        contents3 = file.readlines()
-
-    with open("SARoptimization.py", 'w') as file:
-        for line in contents3:
-            if line.startswith("mtow = "):
-                file.write(f"mtow = {MTOW}\n")
-            else:
-                file.write(line)
-
     # Update variables.py with the new MTOW, AR, S, and e values in one pass
     with open("variables.py", 'r') as file:
         lines = file.readlines()
+    
     with open("variables.py", 'w') as file:
         for line in lines:
             if line.startswith("MTOW = "):
@@ -59,9 +45,5 @@ for i in range(20):
     # Run each file with the updated parameters
     for filename in files_tobe_Updated:
         subprocess.run(["python", filename])
-
-    OEW_est = update_OEW(MTOW)
-
-    # Here you could optionally reload or update OEW_est if it needs to be recalculated.
 
 print(Weights)
