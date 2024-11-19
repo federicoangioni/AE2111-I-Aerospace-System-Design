462842706e53d6632ce13f771257d536febdc84d
medha <agarwal.medhansh@icloud.com>
2024-11-19 15:24:40 +0100

    wef

--- WP4-5/wp4-1/XFLR5sim.py
+++ WP4-5/wp4-1/XFLR5sim.py
@@ -1,2 +1,12 @@
 import numpy as np
 
+file_name = "MainWing_a=0.00_v=10.00ms.txt"
+
+data = np.genfromtxt(
+    file_name,
+    skip_header=30,  # Adjust this based on where the spanwise coefficients start
+    invalid_raise=False,
+    delimiter=None,  # Space or tab delimiter
+)
+
+print(data)
