15827e1ffdb3e018d618f6c59fd0f7d0ac61ad8e
winston1010 <w.z.h.wang@student.tudelft.nl>
2024-11-28 17:15:45 +0100

    Mom graph

--- WP4-5/wp4_2.py
+++ WP4-5/wp4_2.py
@@ -110,6 +110,18 @@ class WingBox():
         I_total_yy = I_wingbox_yy + I_yy_stringers_steiner
 
         return (I_total_xx, I_total_yy)
+    
+     def MOMplots(self, z):
+        ts = [0.001, 0.002, 0.003, 0.004, 0.005]
+        z = np.linspace(0, self.tiplocation)
+        
+        for t in range(len(ts)):
+            plt.plot(z, self.MOM_total(z, ts[t]))        
+        
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+        return plt.gcf()
 
     def polar (self, z, t): # T : torsion, 
         a, b, h, alpha = self.geometry(z)
