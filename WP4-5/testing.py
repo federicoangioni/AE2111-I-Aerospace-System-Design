6f686ec9f6c55f54549208daeb51be90a3e280eb
Federico Angioni <f.angioni@student.tudelft.nl>
2024-11-28 17:44:27 +0100

    changes

--- WP4-5/testing.py
+++ WP4-5/testing.py
@@ -5,9 +5,11 @@ import numpy as np
 
 wingbox = WingBox(0.001, c_r = c_r, c_t = None, wingspan= b, intersection= 0.102, tr = tr)
 
-
+stringers = [6, 0.9, 'L', {'base': 10e-3, 'height': 5e-3, 'thickness base': 2e-3, 'thickness height': 2e-3}]
+print(wingbox.stringer_geometry(0, stringers= stringers))
 
 
 z = np.linspace(0, 1)
 
-wingbox.Jplots(z)
+wingbox.show_geometry(12.078)
+# wingbox.Jplots(z)
