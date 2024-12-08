36cdc000410e34619c26d6a647dea6eabc1fdc8e
MedBox19 <agarwal.medhansh@icloud.com>
2024-12-08 23:15:52 +0100

    UpdateMeddymain.py

--- WP4-5/main.py
+++ WP4-5/main.py
@@ -4,20 +4,16 @@ from wp4_2 import WingBox
 
 xflr_files = 'XFLRdata\\XFLR5sims'
 
-# change this to any angle of attack you'd like to use
-aoa = 2
+# change these
+aircraft_mass = 35688
+alt_sound_speed = 296.56
 
-# aerodynamics = Aerodynamics(folder= xflr_files, aoa= aoa, wingspan= b)
-aerodynamics = Aerodynamics(folder= xflr_files, aoa= aoa, wingspan= 26.9, fus_radius=fus_radius)
-
-g_cl, g_cd, g_cm = aerodynamics.coefficients(return_list= False)
-
-internal_forces = InternalForces(load_factor= load_factor, sound_speed=343, half_chord_sweep= hchord_sweep, fus_radius=fus_radius, density=rho0, airspeed= airspeed, distributions= [g_cl, g_cd, g_cm], 
+internal_forces = InternalForces(aircraft_mass=aircraft_mass, load_factor= load_factor, sound_speed=alt_sound_speed, half_chord_sweep= hchord_sweep, fus_radius=fus_radius, density=rho0, airspeed= airspeed, 
                                  c_r= c_r, wingspan= b, engine_z_loc= engine_z_loc, engine_length= engine_length, x_hl= x_hl, x_lemac= x_lemac, MAC= MAC, 
                                  one_engine_thrust= one_engine_thrust, fan_cowl_diameter= fan_cowl_diameter, c_t= c_r*tr)
     
     
-g_shear, g_moment, g_torque, g_axial = internal_forces.internal_force_diagrams(engine_mass=engine_mass, wing_box_length=wing_box_length, 
+g_shear, g_moment, g_torque, g_axial = internal_forces.force_diagrams(engine_mass=engine_mass, wing_box_length=wing_box_length, 
                                         fuel_tank_length=fuel_tank_length, fuel_density=fuel_density, return_list=False)
 
 # Plotting the internal distribution functions
