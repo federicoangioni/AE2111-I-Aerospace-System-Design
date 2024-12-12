cf92df123de3a5c063f887832adae4dfd51a27bb
Federico Angioni <f.angioni@student.tudelft.nl>
2024-12-12 16:23:45 +0100

    push

--- WP4-5/wp5_1.py
+++ WP4-5/wp5_1.py
@@ -3,47 +3,42 @@ import numpy as np
 from variables import *
 
 
-def SkinBucklingConstant(aspect_ratio):
 
-    # Given points
-    x_points = [0.9, 1, 1.5, 2, 3, 4, 5]
-    y_points = [14, 10.25, 8.75, 8, 7.5, 7.5, 7.5]
+class Buckling():
+    def SkinBucklingConstant(aspect_ratio):
 
-    # Create linear interpolating function
-    linear_interp = interp1d(x_points, y_points, kind='linear')
+        # Given points
+        x_points = [0.9, 1, 1.5, 2, 3, 4, 5]
+        y_points = [14, 10.25, 8.75, 8, 7.5, 7.5, 7.5]
 
-    # Generate interpolated values
-    x_vals = np.linspace(min(x_points), max(x_points), 500)
-    y_vals = linear_interp(x_vals)
+        # Create linear interpolating function
+        linear_interp = interp1d(x_points, y_points, kind='linear')
 
-    # Plotting
-    import matplotlib.pyplot as plt
-    plt.scatter(x_points, y_points, color='red', label='Given Points')
-    plt.plot(x_vals, y_vals, label='Linear Interpolation', color='orange')
-    plt.title('Linear Interpolation')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.legend()
-    plt.grid()
-    plt.show()
+        # Generate interpolated values
+        x_vals = np.linspace(min(x_points), max(x_points), 500)
+        y_vals = linear_interp(x_vals)
 
-    Kc = interp_function(aspect_ratio)
-    print(f"Interpolated value at AR={x_query}: kc={y_query}")
-    return Kc
+        # Plotting
+        import matplotlib.pyplot as plt
+        plt.scatter(x_points, y_points, color='red', label='Given Points')
+        plt.plot(x_vals, y_vals, label='Linear Interpolation', color='orange')
+        plt.title('Linear Interpolation')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend()
+        plt.grid()
+        plt.show()
 
-def SkinAspectRatio(number_of_ribs, wing_span, gino):
-    number_of_panels = number_of_ribs - 1
-    length_of_the_panel = wing_span/number_of_panels
-    length_of_ribs = []
-    panel_area = []
+        Kc = interp_function(aspect_ratio)
+        print(f"Interpolated value at AR={x_query}: kc={y_query}")
+        return Kc
 
-    for i in range(number_of_ribs + 1):
-        a, b, h, alpha = gino(length_of_panel * i)
-        length_of_ribs.append(h)
+    def SkinAspectRatio(number_of_ribs, wing_span, gino):
+        number_of_panels = number_of_ribs - 1
+        length_of_the_panel = wing_span/number_of_panels
 
-    for i in range(number_of_ribs - 1):
-        panel_area.append( 0.5 * length_of_panel * (length_of_ribs[i] + length_of_ribs[i + 1]))
+        for (int i =0, i<=number_of_ribs)
 
+        a, b, h, alpha = gino(length_of_the_panel)
 
-    
-
+        length_of_ribs = 
