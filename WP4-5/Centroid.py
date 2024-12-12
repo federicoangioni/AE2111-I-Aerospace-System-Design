c4866f959576243f15cfabd6feda2a4c1b472432
winston1010 <w.z.h.wang@student.tudelft.nl>
2024-12-12 15:27:30 +0100

    test

---
+++ WP4-5/Centroid.py
@@ -0,0 +1,30 @@
+from variables import *
+from wp4_1 import Aerodynamics, InternalForces
+from wp4_2 import WingBox
+
+
+xflr_files = 'XFLRdata\\XFLR5sims'
+
+# change these
+aircraft_mass = 35688
+alt_sound_speed = 296.56
+
+# internal_forces = InternalForces(aircraft_mass=aircraft_mass, load_factor= load_factor, sound_speed=alt_sound_speed, half_chord_sweep= hchord_sweep, fus_radius=fus_radius, density=rho0, airspeed= airspeed, 
+#                                  c_r= c_r, wingspan= b, engine_z_loc= engine_z_loc, engine_length= engine_length, x_hl= x_hl, x_lemac= x_lemac, MAC= MAC, 
+#                                  one_engine_thrust= one_engine_thrust, fan_cowl_diameter= fan_cowl_diameter, c_t= c_r*tr)
+    
+    
+# g_shear, g_moment, g_torque, g_axial = internal_forces.force_diagrams(engine_mass=engine_mass, wing_box_length=wing_box_length, 
+#                                         fuel_tank_length=fuel_tank_length, fuel_density=fuel_density)[4:]
+
+
+wingbox = WingBox(c_r= c_r, c_t = None, wingspan=b, area_factor_flanges=12, intersection= intersection, tr= tr, t_spar= 0.008, t_caps= 0.004)
+
+stringers = [8, 1, 'I', {'base': 30e-3, 'top': 30e-3, 'web height':30e-3, 'thickness top':2e-3, 'thickness web':2e-3, 'thickness base': 2e-3, }]
+# if I stringer chosen use this
+# [20, 1, 'L', {'base': 30e-3, 'height': 30e-3, 'thickness base': 2e-3, 'thickness height': 2e-3}]
+# wingbox.show(wingbox.z, loads= [g_moment, g_torque], moduli= [E, G], stringers= stringers, limits=[0, 10], plot= True, degrees= True, 
+            #  save=False, filename=None)
+# if you wanna save the plot just set save=True and put some filename.png i.e. filename= 'thickness2.png'
+
+wingbox.show_geometry(12, stringers)
