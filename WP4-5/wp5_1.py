b97a8b3624bdacdba4cfa5b3f0128b03dbe4ece2
Giorgos <georgiosantonopoulos789@gmail.com>
2024-12-16 17:56:02 +0200

    mmm

--- WP4-5/wp5_1.py
+++ WP4-5/wp5_1.py
@@ -103,13 +103,5 @@ class RibWebBuckling():
 
 
 
-    def back_spar_web_buckling(self, a_over_b, E, t_sparweb, b):
-        k_s_array_np = np.array(k_s_array)
-        ab_values = k_s_array_np[:, 0]
-        k_s_values = k_s_array_np[:, 1]
-        k_s = np.interp(a_over_b, ab_values, k_s_values)
-        crit_stress = np.pi**2 * k_s * E /(12*(1-0.33**2)) * (t_sparweb/b)**2
-
-
 
 
