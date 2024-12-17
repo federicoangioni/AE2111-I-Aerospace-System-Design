39f6754a098346aa50cffb04a10872160b12f5fe
Giorgos <georgiosantonopoulos789@gmail.com>
2024-12-17 15:24:16 +0200

    push

--- WP4-5/wp5_1.py
+++ WP4-5/wp5_1.py
@@ -119,3 +119,37 @@ class SkinBuckling():
 
 class RibWebBuckling():
     print("hello world")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def back_spar_web_buckling(self, a_over_b, E, t_sparweb, b):
+        k_s_array_np = np.array(k_s_array)
+        ab_values = k_s_array_np[:, 0]
+        k_s_values = k_s_array_np[:, 1]
+        k_s = np.interp(a_over_b, ab_values, k_s_values)
+        crit_stress = np.pi**2 * k_s * E /(12*(1-0.33**2)) * (t_sparweb/b)**2
+        print('Critical stress is', crit_stress)
