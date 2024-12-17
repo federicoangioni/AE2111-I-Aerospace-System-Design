767d60a623126e2d8aecb26daa7c35dc167c0bd6
Federico Angioni <f.angioni@student.tudelft.nl>
2024-12-17 16:39:04 +0100

    push

--- WP4-5/testing.py
+++ WP4-5/testing.py
@@ -3,11 +3,12 @@ from variables import *
 from wp4_1 import Aerodynamics, InternalForces
 from wp4_2 import WingBox
 from wp5_1 import SkinBuckling
-
+import matplotlib.pyplot as plt
 
 
 wb = WingBox(c_r= c_r, c_t = None, wingspan=b, area_factor_flanges=12, intersection= intersection, tr= tr, t_spar= 0.003, t_caps= 0.002)
 
+#print(wb.geometry(wb.wingspan/2))
+skin_buckling = SkinBuckling(n_ribs=25, wingbox_geometry=wb.geometry, wingspan=wb.wingspan, E= E, v=0.33, t_skin=0.001)
 
-skin_buckling = SkinBuckling(3, wb.geometry, wb.wingspan)
-print(skin_buckling.skin_AR())
+skin_buckling.plot_sigma_cr()
